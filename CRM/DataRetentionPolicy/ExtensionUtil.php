69b9752d3b48cb31acfab51073388f001deb0bef
Darren Woods <dwoods@encircle.co.uk>
2025-09-30 12:02:08 +0100

    Handle ts fallback when extension translate unavailable

--- CRM/DataRetentionPolicy/ExtensionUtil.php
+++ CRM/DataRetentionPolicy/ExtensionUtil.php
@@ -17,7 +17,25 @@ class CRM_DataRetentionPolicy_ExtensionUtil {
 class E extends CRM_Extension_System {
 
   public static function ts($text, $params = []) {
-    return CRM_Extension_System::singleton()->translate(self::LONG_NAME, $text, $params);
+    $extensionSystem = CRM_Extension_System::singleton();
+
+    if (method_exists($extensionSystem, 'translate')) {
+      return $extensionSystem->translate(
+        CRM_DataRetentionPolicy_ExtensionUtil::LONG_NAME,
+        $text,
+        $params
+      );
+    }
+
+    if (!is_array($params)) {
+      $params = (array) $params;
+    }
+
+    if (!array_key_exists('domain', $params)) {
+      $params['domain'] = CRM_DataRetentionPolicy_ExtensionUtil::LONG_NAME;
+    }
+
+    return ts($text, $params);
   }
 
 }
